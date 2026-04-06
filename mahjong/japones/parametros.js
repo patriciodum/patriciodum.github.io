517f6d19673088da7f8754a8de9e67a6c3655238
patriciodum <123562438+patriciodum@users.noreply.github.com>
2026-04-06 10:16:37 -0300

    Agregado parametro de Zoom y Wide

---
+++ mahjong/japones/parametros.js
@@ -0,0 +1,24 @@
+//getParametro
+function getParametro(variable) {
+		var query = window.location.search.substring(1);
+		var vars = query.split('&');
+		for (var i = 0; i < vars.length; i++) {
+			var pair = vars[i].split('=');
+			if (decodeURIComponent(pair[0]) == variable) {
+				return decodeURIComponent(pair[1]);
+			}
+		}
+		console.log('No se encontro el parametro %s', variable);
+	}
+	
+	function hayParametro(variable) {
+		var query = window.location.search.substring(1);
+		var vars = query.split('&');
+		for (var i = 0; i < vars.length; i++) {
+			var pair = vars[i].split('=');
+			if (decodeURIComponent(pair[0]) == variable) {
+				return true;
+			}
+		}
+		return false;
+	}
